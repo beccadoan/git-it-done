b5d55f2ee8ef89f2973abc6a5ab104e78cd8627a
Becca Doan <rebecca.doan@yahoo.com>
2022-06-12 12:09:22 -0500

    finished issues page

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,17 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
+
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, ";
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append warning to container
+    limitWarningEl.appendChild(linkEl);
+}
 
 var displayIssues = function(issues){
     if (issues.length === 0) {
@@ -42,7 +55,12 @@ var getRepoIssues = function(repo) {
     fetch(apiUrl).then(function(response) {
         if (response.ok) {
             response.json().then(function(data){
-                displayIssues(data)
+                displayIssues(data);
+
+                // check if api has paginated issues
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
             })
         } else {
             alert("There was a problem with your request!")
